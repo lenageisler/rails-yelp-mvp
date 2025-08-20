c31dcb3049569cae923b2a662531ec7072174040
Lena Geisler <lenageisler@gmx.de>
2025-08-20 12:13:37 +0200

    added routes and controllers for restaurants and reviews

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  
 end
